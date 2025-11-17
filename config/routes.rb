3510d8edcd672254dbb036894c05625eb6f21449
dhq_boiler <dhq_boiler@live.jp>
2025-11-17 13:24:54 +0900

    Remove

--- config/routes.rb
+++ config/routes.rb
@@ -4,34 +4,12 @@ RailsImagePostSolution::Engine.routes.draw do
   # User-facing report API
   resources :image_reports, only: [ :create ]
 
-  # Admin dashboard
+  # Admin dashboard (engine controllers)
   namespace :admin do
     resources :image_reports, only: %i[index show] do
       member do
         patch :confirm
         patch :dismiss
-      end
-    end
-
-    # User management
-    resources :users, only: %i[index show] do
-      member do
-        post :suspend
-        post :unsuspend
-        post :ban
-        post :unban
-      end
-    end
-
-    # Frozen posts management
-    resources :frozen_posts, only: [:index] do
-      collection do
-        post "unfreeze_stage/:id", to: "frozen_posts#unfreeze_stage", as: :unfreeze_stage
-        post "unfreeze_comment/:id", to: "frozen_posts#unfreeze_comment", as: :unfreeze_comment
-        post "permanent_freeze_stage/:id", to: "frozen_posts#permanent_freeze_stage", as: :permanent_freeze_stage
-        post "permanent_freeze_comment/:id", to: "frozen_posts#permanent_freeze_comment", as: :permanent_freeze_comment
-        delete "destroy_stage/:id", to: "frozen_posts#destroy_stage", as: :destroy_stage
-        delete "destroy_comment/:id", to: "frozen_posts#destroy_comment", as: :destroy_comment
       end
     end
   end
