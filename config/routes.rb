59c47b09136d10175bec9124ce3216b040fe26f9
dhq_boiler <dhq_boiler@live.jp>
2025-11-17 17:57:15 +0900

    Bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,41 @@
 # frozen_string_literal: true
 
 RailsImagePostSolution::Engine.routes.draw do
-  # Support optional locale parameter to match host application's routing
-  # Accept any 2-letter locale code to be flexible with host app's locales
-  scope "(:locale)", locale: /[a-z]{2}/ do
-    # User-facing report API
-    resources :image_reports, only: [ :create ]
+  # Locale parameter is handled by the host application at mount level
+  # No need for locale scope within the engine itself
 
-    # Admin dashboard (engine controllers)
-    namespace :admin do
-      # Image reports management
-      resources :image_reports, only: %i[index show] do
-        member do
-          patch :confirm
-          patch :dismiss
-        end
+  # User-facing report API
+  resources :image_reports, only: [ :create ]
+
+  # Admin dashboard (engine controllers)
+  namespace :admin do
+    # Image reports management
+    resources :image_reports, only: %i[index show] do
+      member do
+        patch :confirm
+        patch :dismiss
       end
+    end
 
-      # User management
-      resources :users, only: %i[index show] do
-        member do
-          post :suspend
-          post :unsuspend
-          post :ban
-          post :unban
-        end
+    # User management
+    resources :users, only: %i[index show] do
+      member do
+        post :suspend
+        post :unsuspend
+        post :ban
+        post :unban
       end
+    end
 
-      # Frozen posts management
-      resources :frozen_posts, only: [ :index ] do
-        collection do
-          post "unfreeze_stage/:id", to: "frozen_posts#unfreeze_stage", as: :unfreeze_stage
-          post "unfreeze_comment/:id", to: "frozen_posts#unfreeze_comment", as: :unfreeze_comment
-          post "permanent_freeze_stage/:id", to: "frozen_posts#permanent_freeze_stage", as: :permanent_freeze_stage
-          post "permanent_freeze_comment/:id", to: "frozen_posts#permanent_freeze_comment", as: :permanent_freeze_comment
-          delete "destroy_stage/:id", to: "frozen_posts#destroy_stage", as: :destroy_stage
-          delete "destroy_comment/:id", to: "frozen_posts#destroy_comment", as: :destroy_comment
-        end
+    # Frozen posts management
+    resources :frozen_posts, only: [ :index ] do
+      collection do
+        post "unfreeze_stage/:id", to: "frozen_posts#unfreeze_stage", as: :unfreeze_stage
+        post "unfreeze_comment/:id", to: "frozen_posts#unfreeze_comment", as: :unfreeze_comment
+        post "permanent_freeze_stage/:id", to: "frozen_posts#permanent_freeze_stage", as: :permanent_freeze_stage
+        post "permanent_freeze_comment/:id", to: "frozen_posts#permanent_freeze_comment", as: :permanent_freeze_comment
+        delete "destroy_stage/:id", to: "frozen_posts#destroy_stage", as: :destroy_stage
+        delete "destroy_comment/:id", to: "frozen_posts#destroy_comment", as: :destroy_comment
       end
     end
   end
