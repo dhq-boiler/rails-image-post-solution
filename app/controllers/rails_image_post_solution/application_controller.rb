899845dc5ce81853561f33a9e850678625bdb8db
dhq_boiler <dhq_boiler@live.jp>
2025-11-17 16:29:28 +0900

    Override authentication methods to use main_app routes

--- app/controllers/rails_image_post_solution/application_controller.rb
+++ app/controllers/rails_image_post_solution/application_controller.rb
@@ -4,5 +4,24 @@ module RailsImagePostSolution
   class ApplicationController < ::ApplicationController
     # Engine's base controller inherits from host application's ApplicationController
     # This allows the engine to use the host app's authentication methods
+
+    # Override require_login to use main_app routes
+    def require_login
+      unless logged_in?
+        redirect_to main_app.login_path, alert: I18n.t("errors.messages.login_required")
+      end
+    end
+
+    # Add require_admin method for admin controllers
+    def require_admin
+      unless logged_in?
+        redirect_to main_app.login_path, alert: I18n.t("errors.messages.login_required")
+        return
+      end
+
+      unless current_user.admin?
+        redirect_to main_app.root_path, alert: I18n.t("errors.messages.admin_required")
+      end
+    end
   end
 end
